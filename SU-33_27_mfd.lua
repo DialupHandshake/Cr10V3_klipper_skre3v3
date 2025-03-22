01c0bd58f6303274830411652787d513beed98f3
DialupHandshake <mrtambalov@gmail.com>
2025-03-22 23:33:56 +0000

    Create SU-33_27_mfd.lua

---
+++ SU-33_27_mfd.lua
@@ -0,0 +1,39 @@
+_  = function(p) return p; end;
+name = _('su_33_27_nav');
+Description = 'Right MFCD on the smaller vertical screen'
+Viewports =
+{
+     Center =
+     {
+          x = 0;
+          y = 0;
+          width  = 1920;
+          height = 1080;
+          viewDx = 0;
+          viewDy = 0;
+          aspect = 1920/1080;
+     }
+}
+
+RIGHT_MFCD =
+{
+     x = 576;
+     y = 1080;
+     width = 768;
+     height = 768;
+}
+
+UIMainView = 
+{
+	x = 0;
+	y = 0;
+	width = 1920;
+	height = 1080;
+}
+GU_MAIN_VIEWPORT = 
+{
+	x = 0;
+	y = 0;
+	width = 1920;
+	height = 1080;
+}
