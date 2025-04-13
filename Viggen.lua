eebb6f869f73da90e8b105eef9d50e862ea39b12
DialupHandshake <mrtambalov@gmail.com>
2025-04-13 22:28:41 +0100

    Create Viggen.lua

---
+++ Viggen.lua
@@ -0,0 +1,48 @@
+_  = function(p) return p; end;
+name = _('Two_mfd_1920_1848');
+Description = 'Left and right MFD on mini monitor'
+Viewports =
+{
+     Center =
+     {
+          x = 0;
+          y = 0;
+          width = 1920;
+          height = 1080;
+          viewDx = 0;
+          viewDy = 0;
+          aspect = 1920/1080;
+     }
+}
+
+LEFT_MFCD =
+{
+     x = 448;
+     y = 1080;
+     width = 512;
+     height = 512;
+}
+
+RIGHT_MFCD =
+{
+     x = 960;
+     y = 1080;
+     width = 512;
+     height = 512;
+}
+
+UIMainView = 
+{
+	x = 0;
+	y = 0;
+	width = 1920;
+	height = 1080;
+}
+
+GU_MAIN_VIEWPORT = 
+{
+	x = 0;
+	y = 0;
+	width = 1920;
+	height = 1080;
+}
